2aad0796c5a46c787723229adadbfc0bc055eb08
AyselHavutcu <42868243+AyselHavutcu@users.noreply.github.com>
2023-03-10 15:17:23 +0300

    Label Ids color code resolving code added. Additional fixes.

--- client.py
+++ client.py
@@ -24,38 +24,30 @@ def generate_excel(args):
     data = response.json()
     
     res_df = pd.DataFrame(data)
-    
+
     # Sort dataframe by "gruppe" column
     res_df.sort_values('gruppe', inplace=True)
     
+    
     # Define columns to include in output
     keys = ['rnr'] + args.keys if args.keys else ['rnr']
-    if 'labelIds' in keys and args.colored:
-        keys.remove('labelIds')
+
     if args.colored:
-        colors = {
-            'green': '#007500',
-            'orange': '#FFA500',
-            'red': '#b30000'
-        }
-        def get_color(row):
-            diff = datetime.today() - datetime.strptime(row['hu'], '%Y-%m-%d')
-            if diff.days <= 90:
-                return colors['green']
-            elif diff.days <= 365:
-                return colors['orange']
-            else:
-                return colors['red']
-        res_df['color'] = res_df.apply(get_color, axis=1)
-        res_df = res_df.style.applymap(lambda x: f'background-color: {x}', subset=pd.IndexSlice[:, ['color']])
+        res_df = color_cells_with_hu(res_df)
         style = True
     else:
         style = None
 
+     # If labelIds are given and at least one colorCode could be resolved,
+    # use the first colorCode to tint the cell's text (if labelIds is given in -k)
+    if 'labelIds' in keys:
+        res_df = color_cells_with_labelIds(res_df,keys)
+ 
+
     # Select only the desired columns and write to Excel file
     filename = 'vehicles_{}.xlsx'.format(datetime.now().strftime('%Y-%m-%d'))
     res_df.to_excel(filename, index=False)
-
+    
     # Apply cell coloring if the "-c" flag is True
     if style is not None:
         # Create Excel file and save DataFrame as a worksheet in the workbook
@@ -65,6 +57,37 @@ def generate_excel(args):
 
         # Save the Excel file
         writer.save()
+
+
+def color_cells_with_hu(res_df):
+    colors = {
+            'green': '#007500',
+            'orange': '#FFA500',
+            'red': '#b30000'
+        }
+    def get_color(row):
+            diff = datetime.today() - datetime.strptime(row['hu'], '%Y-%m-%d')
+
+            if diff.days <= 90:
+                return colors['green']
+            elif diff.days <= 365:
+                return colors['orange']
+            else:
+                return colors['red']
+
+    res_df['color'] = res_df.apply(get_color, axis=1)
+    res_df = res_df.style.applymap(lambda x: f'background-color: {x}', subset=pd.IndexSlice[:, ['color']])
+
+    return res_df
+
+
+def color_cells_with_labelIds(res_df,keys):
+    color_codes = list(set(res_df['colorCode']))
+    if len(color_codes) > 0:
+        color_code = color_codes[0]
+        res_df['labelIds'] = res_df['labelIds'].apply(lambda x: f'\033[38;5;{color_code}m{x}\033[0m' if x else '')
+    string_df = res_df.to_string(index=False, columns=keys)
+    return string_df
 
 
 # Define input arguments
